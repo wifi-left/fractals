e1fe2929fe2957bb1076dfd2a3a7f62c6aca9406
frosty <alp.waterfall@gmail.com>
2025-10-25 20:47:59 +0100

    - So this method is using custom trees, and causes clipping if the tree generates beyond neighbouring chunks.
    - Because of this I have decided to stop this method and pivot to using structures. This will likely be the end of this branch.

--- src/main/java/net/frosty/fractals/world/tree/custom/FractalTrunkPlacer.java
+++ src/main/java/net/frosty/fractals/world/tree/custom/FractalTrunkPlacer.java
@@ -13,6 +13,7 @@ import net.minecraft.util.math.random.Random;
 import net.minecraft.world.TestableWorld;
 import net.minecraft.world.gen.feature.TreeFeatureConfig;
 import net.minecraft.world.gen.foliage.FoliagePlacer;
+import net.minecraft.world.gen.stateprovider.BlockStateProvider;
 import net.minecraft.world.gen.trunk.TrunkPlacer;
 import net.minecraft.world.gen.trunk.TrunkPlacerType;
 import org.spongepowered.include.com.google.common.collect.ImmutableList;
@@ -23,6 +24,7 @@ import java.util.List;
 import java.util.function.BiConsumer;
 
 public class FractalTrunkPlacer extends TrunkPlacer {
+
     // Use the fillTrunkPlacerFields to create our codec
     public static final MapCodec<FractalTrunkPlacer> CODEC = RecordCodecBuilder.mapCodec(instance ->
             fillTrunkPlacerFields(instance).apply(instance, FractalTrunkPlacer::new));
@@ -38,6 +40,13 @@ public class FractalTrunkPlacer extends TrunkPlacer {
 
     @Override
     public List<FoliagePlacer.TreeNode> generate(TestableWorld world, BiConsumer<BlockPos, BlockState> replacer, Random random, int height, BlockPos startPos, TreeFeatureConfig config) {
+
+        TreeFeatureConfig leafConfig = new TreeFeatureConfig.Builder(
+                BlockStateProvider.of(Blocks.OAK_LEAVES),config.trunkPlacer,
+                config.foliageProvider,config.foliagePlacer,
+                config.minimumSize
+        ).build();
+
         // Set the ground beneath the trunk to dirt
         setToDirt(world, replacer, random, startPos.down(), config);
 
@@ -71,6 +80,10 @@ public class FractalTrunkPlacer extends TrunkPlacer {
         // Iterate until the trunk height limit and place two blocks using the getAndSetState method from TrunkPlacer
         for (BlockPos bp: toEdit) {
             this.getAndSetState(world, replacer, random, bp, config);
+        }
+
+        for (BlockPos bp: toLeaf) {
+            this.getAndSetState(world, replacer, random, bp, leafConfig);
         }
 
         // We create two TreeNodes - one for the first trunk, and the other for the second
